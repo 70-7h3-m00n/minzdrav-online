217fd31b2d79ea085984b2d09a4a00a5ed670e39
Zhenya <asgyrdjons@gmail.com>
2023-05-29 15:31:43 +0300

    fix: test4.

--- src/features/FormApplication/components/FormSending/index.tsx
+++ src/features/FormApplication/components/FormSending/index.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.scss'
 import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
 import { emailExp, phoneExp } from '@/src/shared/regExp'
-import {Controller, SubmitHandler, useForm} from 'react-hook-form'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import classNames from 'classnames'
 
 interface FormSending {
@@ -38,65 +38,70 @@ const FormSending = (): JSX.Element => {
                 <label className={styles.label}>
                     <p className={styles.descrTextValid}>{errors.name?.message}</p>
 
-                    <Controller render={({field}) => (
-                                    <input {...field}
-                                           className={'name' in errors ? styles.valid : styles.input}
-                                           placeholder={errors.name?.message || t('name')!}
-                                    />
-                                )}
-                                rules={{
-                                    required: t('validName')!,
-                                    minLength: {
-                                        value: 2,
-                                        message: 'Минимальная длинна 2 символа'
-                                    }
-                                }}
-                                name={'name'}
-                                control={control}
+                    <Controller
+                        render={({ field }) => (
+                            <input
+                                {...field}
+                                className={'name' in errors ? styles.valid : styles.input}
+                                placeholder={errors.name?.message || t('name')!}
+                            />
+                        )}
+                        rules={{
+                            required: t('validName')!,
+                            minLength: {
+                                value: 2,
+                                message: 'Минимальная длинна 2 символа',
+                            },
+                        }}
+                        name={'name'}
+                        control={control}
                     />
-
                 </label>
 
                 <div className={styles.flexBlock}>
                     <label className={classNames(styles.label, styles.inputWidth)}>
                         <p className={styles.descrTextValid}>{errors.phone?.message}</p>
 
-                        <Controller render={({field}) => (
-                                        <input {...field}
-                                               className={'phone' in errors ? styles.valid : styles.input}
-                                               placeholder={errors.phone?.message || t('phone')!}
-                                        />
-                                    )}
-                                    rules={{
-                                        required: t('validPhone')!,
-                                        pattern: {
-                                            value: phoneExp,
-                                            message: 'Телефон не соотвествует номерам телефона РФ',
-                                        },
-                                    }}
-                                    name={'phone'}
-                                    control={control}
+                        <Controller
+                            render={({ field }) => (
+                                <input
+                                    {...field}
+                                    className={'phone' in errors ? styles.valid : styles.input}
+                                    placeholder={errors.phone?.message || t('phone')!}
+                                />
+                            )}
+                            rules={{
+                                required: t('validPhone')!,
+                                pattern: {
+                                    value: phoneExp,
+                                    message: 'Телефон не соотвествует номерам телефона РФ',
+                                },
+                            }}
+                            name={'phone'}
+                            control={control}
                         />
                     </label>
 
                     <label className={classNames(styles.label, styles.inputWidth)}>
                         <p className={styles.descrTextValid}>{errors.email?.message}</p>
 
-                        <Controller render={({field}) => (
-                            <input {...field}
-                                   className={'email' in errors ? styles.valid : styles.input}
-                                   placeholder={errors.email?.message || t('email')!}
-                            />
-                        )}
-                                    rules={{
-                                        required: t('validEmail')!,
-                                        pattern: {
-                                            value: emailExp,
-                                            message: 'email введен не коректно ',
-                                        },
-                                    }}
-                                    name={'email'}
-                                    control={control}
+                        <Controller
+                            render={({ field }) => (
+                                <input
+                                    {...field}
+                                    className={'email' in errors ? styles.valid : styles.input}
+                                    placeholder={errors.email?.message || t('email')!}
+                                />
+                            )}
+                            rules={{
+                                required: t('validEmail')!,
+                                pattern: {
+                                    value: emailExp,
+                                    message: 'email введен не коректно ',
+                                },
+                            }}
+                            name={'email'}
+                            control={control}
                         />
                     </label>
                 </div>

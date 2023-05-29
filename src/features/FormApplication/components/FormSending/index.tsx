1c2fac8687610c1fd3b3cc7123fbd700a049493a
Zhenya <asgyrdjons@gmail.com>
2023-05-29 14:55:56 +0300

    fix: test3.

--- src/features/FormApplication/components/FormSending/index.tsx
+++ src/features/FormApplication/components/FormSending/index.tsx
@@ -2,9 +2,8 @@ import styles from './styles.module.scss'
 import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
 import { emailExp, phoneExp } from '@/src/shared/regExp'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import {Controller, SubmitHandler, useForm} from 'react-hook-form'
 import classNames from 'classnames'
-import { useEffect, useState } from 'react'
 
 interface FormSending {
     name: string
@@ -13,10 +12,9 @@ interface FormSending {
 }
 
 const FormSending = (): JSX.Element => {
-    const [clientRender, setClientRender] = useState(false)
     const { t } = useTranslation('form')
     const {
-        register,
+        control,
         formState: { errors, isValid },
         handleSubmit,
         reset,
@@ -29,14 +27,6 @@ const FormSending = (): JSX.Element => {
         mode: 'onTouched',
     })
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setClientRender(true)
-        }
-    }, [])
-
-    if (!clientRender) return <></>
-
     const submit: SubmitHandler<FormSending> = data => {
         console.log(data)
         reset()
@@ -48,49 +38,65 @@ const FormSending = (): JSX.Element => {
                 <label className={styles.label}>
                     <p className={styles.descrTextValid}>{errors.name?.message}</p>
 
-                    <input
-                        className={'name' in errors ? styles.valid : styles.input}
-                        placeholder={errors.name?.message || t('name')!}
-                        {...register('name', {
-                            required: t('validName')!,
-                            minLength: {
-                                value: 2,
-                                message: 'Минимальная длинна 2 символа',
-                            },
-                        })}
+                    <Controller render={({field}) => (
+                                    <input {...field}
+                                           className={'name' in errors ? styles.valid : styles.input}
+                                           placeholder={errors.name?.message || t('name')!}
+                                    />
+                                )}
+                                rules={{
+                                    required: t('validName')!,
+                                    minLength: {
+                                        value: 2,
+                                        message: 'Минимальная длинна 2 символа'
+                                    }
+                                }}
+                                name={'name'}
+                                control={control}
                     />
+
                 </label>
 
                 <div className={styles.flexBlock}>
                     <label className={classNames(styles.label, styles.inputWidth)}>
                         <p className={styles.descrTextValid}>{errors.phone?.message}</p>
 
-                        <input
-                            className={'phone' in errors ? styles.valid : styles.input}
-                            placeholder={errors.phone?.message || t('phone')!}
-                            {...register('phone', {
-                                required: t('validPhone')!,
-                                pattern: {
-                                    value: phoneExp,
-                                    message: 'Телефон не соотвествует номерам телефона РФ',
-                                },
-                            })}
+                        <Controller render={({field}) => (
+                                        <input {...field}
+                                               className={'phone' in errors ? styles.valid : styles.input}
+                                               placeholder={errors.phone?.message || t('phone')!}
+                                        />
+                                    )}
+                                    rules={{
+                                        required: t('validPhone')!,
+                                        pattern: {
+                                            value: phoneExp,
+                                            message: 'Телефон не соотвествует номерам телефона РФ',
+                                        },
+                                    }}
+                                    name={'phone'}
+                                    control={control}
                         />
                     </label>
 
                     <label className={classNames(styles.label, styles.inputWidth)}>
                         <p className={styles.descrTextValid}>{errors.email?.message}</p>
 
-                        <input
-                            className={'email' in errors ? styles.valid : styles.input}
-                            placeholder={errors.email?.message || t('email')!}
-                            {...register('email', {
-                                required: t('validEmail')!,
-                                pattern: {
-                                    value: emailExp,
-                                    message: 'email введен не коректно ',
-                                },
-                            })}
+                        <Controller render={({field}) => (
+                            <input {...field}
+                                   className={'email' in errors ? styles.valid : styles.input}
+                                   placeholder={errors.email?.message || t('email')!}
+                            />
+                        )}
+                                    rules={{
+                                        required: t('validEmail')!,
+                                        pattern: {
+                                            value: emailExp,
+                                            message: 'email введен не коректно ',
+                                        },
+                                    }}
+                                    name={'email'}
+                                    control={control}
                         />
                     </label>
                 </div>

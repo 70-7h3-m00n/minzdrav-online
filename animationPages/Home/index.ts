7db1533fa55cca3b5c9bb418fa36f0f50603d65d
Zhenya <asgyrdjons@gmail.com>
2023-06-05 17:44:44 +0300

    feat: Добавление анимации на главную страницу.

---
+++ animationPages/Home/index.ts
@@ -0,0 +1,39 @@
+export const animation = {
+    leftContentAnimation: {
+        hidden: {
+            x: -100,
+            opacity: 0,
+        },
+        visible: (custom: number) => ({
+            x: 0,
+            opacity: 1,
+            transition: { delay: custom * 0.2 },
+        }),
+    },
+    rightContentAnimation: {
+        hidden: {
+            x: 100,
+            opacity: 0,
+        },
+        visible: (custom: number) => ({
+            x: 0,
+            opacity: 1,
+            transition: {
+                delay: custom * 0.2,
+            },
+        }),
+    },
+    bottomContentAnimation: {
+        hidden: {
+            y: 100,
+            opacity: 0,
+        },
+        visible: (custom: number) => ({
+            y: 0,
+            opacity: 1,
+            transition: {
+                delay: custom * 0.3,
+            },
+        }),
+    },
+}

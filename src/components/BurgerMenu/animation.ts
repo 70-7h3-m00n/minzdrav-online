810182d476c8b2a84c5c4641615215be85c28a1d
Zhenya <asgyrdjons@gmail.com>
2023-07-13 12:45:31 +0300

    fix: Исправление стиля в мобильном меню.

--- src/components/BurgerMenu/animation.ts
+++ src/components/BurgerMenu/animation.ts
@@ -10,6 +10,7 @@ export const animation: Animation = {
         open: {
             opacity: 1,
             y: 0,
+            marginTop: 25,
             transition: { type: 'spring', stiffness: 300, damping: 24 },
         },
         closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },

678bd66cf76d08e0ed512a7d6df11270df5ab47f
Zhenya <asgyrdjons@gmail.com>
2023-07-26 01:41:37 +0300

    fix: Убрал блок если масив пустой (Для кого этоот курс).

--- src/components/MetaManifest/index.tsx
+++ src/components/MetaManifest/index.tsx
@@ -3,6 +3,7 @@ const MetaManifest = () => {
         <>
             <meta name='yandex-verification' content='429826b4f03c6627' />
             <link rel='icon' href='/icons/favicon.ico' />
+            <meta name='robots' content='all' />
         </>
     )
 }

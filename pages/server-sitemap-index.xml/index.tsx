cbfe29fa123d147a32a848e2164a399a66fadbe1
Zhenya <asgyrdjons@gmail.com>
2023-07-26 13:05:08 +0300

    fix: test.

---
+++ pages/server-sitemap-index.xml/index.tsx
@@ -0,0 +1,33 @@
+import { getServerSideSitemapIndexLegacy } from 'next-sitemap'
+import { GetServerSideProps } from 'next'
+import fetchPathsCourses from '@/src/api/fetchPathsCourses'
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    const paths = (
+        await Promise.all(
+            ctx.locales!.map(async local => {
+                const data = await fetchPathsCourses(local)
+                return data.map(course => ({
+                    params: { slug: course.pathCourse },
+                    locale: local,
+                }))
+            }),
+        )
+    ).flat(2)
+
+    const fields: string[] = paths.reduce(
+        (previousValue, currentValue) =>
+            previousValue.concat(
+                `https://minzdrav.online${currentValue.locale === 'ru' ? '' : `/${currentValue.locale}`}/courses/${
+                    currentValue.params.slug
+                }`,
+            ),
+        ['https://minzdrav.online/courses'],
+    )
+
+    console.log(fields)
+
+    return getServerSideSitemapIndexLegacy(ctx, fields)
+}
+
+export default function SitemapIndex() {}
--- src/components/MetaManifest/index.tsx
+++ src/components/MetaManifest/index.tsx
@@ -4,6 +4,21 @@ const MetaManifest = () => {
             <meta name='yandex-verification' content='429826b4f03c6627' />
             <link rel='icon' href='/icons/favicon.ico' />
             <meta name='robots' content='all' />
+
+            <meta name='application-name' content='Московский Институт Профессионального Образования' />
+            <meta name='apple-mobile-web-app-capable' content='yes' />
+            <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+            <meta name='apple-mobile-web-app-title' content='Московский Институт Профессионального Образования' />
+            <meta name='mobile-web-app-capable' content='yes' />
+            <meta name='msapplication-tap-highlight' content='no' />
+
+            <meta name={'yandex-verification'} content={'9ff11531d063e792'} />
+            <link rel='apple-touch-icon' sizes='180x180' href='/assets/imgs/icons/apple-icon-180.png' />
+
+            <link rel='manifest' href='/manifest.json' />
+
+            <link rel='icon' href='/favicon.ico' />
+            <link rel='shortcut icon' href='/favicon.ico' />
         </>
     )
 }
